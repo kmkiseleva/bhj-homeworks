41011cc4d32af4f01eb99a9a807402eaa7f41112
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2021-03-11 19:09:54 +0300

    first commit document-structure

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,19 +1,22 @@
 const links = [...document.querySelectorAll("a")];
-const tips = [
-  '<div class="tooltip tooltip_active">Что бы вы хотели?</div>',
-  '<div class="tooltip tooltip_active">Устройтесь на работу!</div>',
-  '<div class="tooltip tooltip_active">Это просто подсказка!</div>',
-  '<div class="tooltip tooltip_active">Не важный текст подсказки</div>',
-  '<div class="tooltip tooltip_active">Подсказка открыта</div>',
-  '<div class="tooltip tooltip_active">Выхода нет, скоро рассвет</div>',
-];
 
-let index;
 for (let link of links) {
+  let index;
   link.addEventListener("click", (e) => {
     e.preventDefault();
     index = links.indexOf(link);
-    console.log(links[index].getBoundingClientRect());
-    links[index].insertAdjacentHTML("afterEnd", tips[index]);
+    const leftBound = link.getBoundingClientRect().left;
+    const bottomBound = link.getBoundingClientRect().bottom;
+    let text = link.getAttribute("title");
+    let nextItem = link.nextElementSibling;
+
+    if (nextItem === null || !nextItem.classList.contains("tooltip_active")) {
+      links[index].insertAdjacentHTML(
+        "afterEnd",
+        `<div class="tooltip tooltip_active" style="left: ${leftBound}px; top: ${bottomBound}px">${text}</div>`
+      );
+    } else if (nextItem.classList.contains("tooltip")) {
+      nextItem.classList.remove("tooltip_active");
+    }
   });
 }
