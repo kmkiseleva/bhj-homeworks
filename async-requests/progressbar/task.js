dc6c43110a83f970482a166224220609f645fa82
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2021-03-13 19:24:47 +0300

    async-requests

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const progress = document.getElementById("progress");
+progress.value = 0.0;
+
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "https://netology-slow-rest.herokuapp.com/upload.php");
+xhr.send();
+xhr.addEventListener("readystatechange", () => {
+  if (xhr.readyState !== xhr.DONE) {
+    progress.value += 0.1;
+  }
+});
