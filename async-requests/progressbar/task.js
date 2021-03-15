c625bc24f67a33f8ae299cafad4ab39ea0c4eddf
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2021-03-15 22:35:14 +0300

    second commit async-requests

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,12 +1,16 @@
 "use strict";
+const form = document.getElementById("form");
 const progress = document.getElementById("progress");
 progress.value = 0.0;
 
-let xhr = new XMLHttpRequest();
-xhr.open("GET", "https://netology-slow-rest.herokuapp.com/upload.php");
-xhr.send();
-xhr.addEventListener("readystatechange", () => {
-  if (xhr.readyState !== xhr.DONE) {
-    progress.value += 0.1;
-  }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://netology-slow-rest.herokuapp.com/upload.php");
+  xhr.send();
+  xhr.addEventListener("readystatechange", () => {
+    if (xhr.readyState !== xhr.DONE) {
+      progress.value += 0.1;
+    }
+  });
 });
